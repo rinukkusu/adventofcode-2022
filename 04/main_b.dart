f6dbf6679abf7a8f13994ea365f7f5cd3fbe1084
Max Riegler <rinukkusu@sub-r.de>
2022-12-04 10:42:12 +0100

    04 - add solutions for the fourth day

---
+++ 04/main_b.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+main() {
+  var assigmentPairsContainingEachOther = new File('data')
+      .readAsLinesSync()
+      .map((x) => x.split(','))
+      .map((x) => [
+            x[0].split('-').map(int.parse).toList(),
+            x[1].split('-').map(int.parse).toList()
+          ])
+      .map((x) => (x[0][0] <= x[1][1] && x[0][1] >= x[1][0] ||
+              x[1][0] <= x[0][1] && x[1][1] >= x[0][0])
+          ? 1
+          : 0)
+      .fold(0, (int previousValue, int element) => previousValue + element);
+
+  print(assigmentPairsContainingEachOther);
+}
